f9f48f6cb6a0fb7d3fdc74ee40b1a226ccd0e5a1
Paulina Wall <paulina_wall@outlook.com>
2023-03-01 13:44:40 +0100

    Add Competitions and common components

---
+++ src/WellnessBingo/WellnessBingo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './WellnessBingo.css';
+import { PageHeader } from '../Common/PageHeader/PageHeader';
+
+export const WellnessBingo = () => (
+  <div>
+    <PageHeader
+      heading="Wellness bingo"
+      description="Complete the assignment and try to get bingo"
+    />
+  </div>
+)
